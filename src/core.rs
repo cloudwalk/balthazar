f9269622941e5cc3af91ba6b67f21d96f42acc30
Cainã Costa <cfcosta@cloudwalk.io>
2022-03-15 09:37:00 -0300

    breaking: use standard name for colors env var

--- src/core.rs
+++ src/core.rs
@@ -2,15 +2,15 @@ use crate::prelude::*;
 
 #[derive(Debug, Clone, Parser)]
 pub struct CoreConfig {
-    #[clap(short, long, env = "DISABLE_COLORS")]
-    pub disable_colors: bool,
+    #[clap(short, long, env = "NO_COLORS")]
+    pub no_colors: bool,
 }
 
 pub struct Core;
 
 impl Feature for Core {
     fn init(config: EnvironmentConfig) -> Result<Self> {
-        if !config.core.disable_colors {
+        if !config.core.no_colors {
             color_eyre::install()?;
         }
 
