666636bfe989689e4145714b7d0d1dac5e83687b
Cainã Costa <cfcosta@cloudwalk.io>
2022-03-29 16:29:10 -0300

    feat: fix weirdness, use Option<Layer> to disable tracing

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,11 +1,18 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+use std::fmt::Debug;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Registry};
 use tracing_tree::HierarchicalLayer;
 
 use crate::{EnvironmentConfig, Feature, Parser, Result};
 
 #[derive(Debug, Clone, Parser)]
 pub struct TracingConfig {
-    #[clap(env = "TRACING_DISABLE_OPENTELEMETRY", parse(try_from_str))]
+    #[clap(
+        long = "tracing-disable-opentelemetry",
+        env = "TRACING_DISABLE_OPENTELEMETRY"
+    )]
     pub disable_opentelemetry: bool,
 
     #[clap(
@@ -16,36 +23,75 @@ pub struct TracingConfig {
 
     #[clap(env = "TRACING_LOG_LEVEL", default_value = "debug")]
     pub log_level: String,
+
+    #[clap(arg_enum, env = "TRACING_FORMAT", default_value = "pretty")]
+    pub format: TracingFormat,
 }
 
-#[derive(Debug, Clone)]
+#[derive(clap::Parser, Debug, Clone)]
 pub struct Tracing;
 
 impl Feature for Tracing {
     fn init(config: EnvironmentConfig) -> Result<Self> {
         std::env::set_var("RUST_LOG", &config.tracing.log_level);
 
-        let registry = Registry::default()
-            .with(EnvFilter::from_default_env())
-            .with(
-                HierarchicalLayer::new(2)
-                    .with_targets(true)
-                    .with_bracketed_fields(true)
-                    .with_ansi(config.core.no_color),
-            );
-
-        if config.tracing.disable_opentelemetry {
-            registry.init();
+        let telemetry = if config.tracing.disable_opentelemetry {
+            None
         } else {
             let tracer = opentelemetry_jaeger::new_pipeline()
                 .with_collector_endpoint(&config.tracing.opentelemetry_endpoint)
                 .with_service_name(module_path!())
                 .install_batch(opentelemetry::runtime::Tokio)?;
+            Some(tracing_opentelemetry::layer().with_tracer(tracer))
+        };
 
-            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+        // tracing_subscriber lib currently does not support dynamically adding layer to registry
+        // accordingly to some condition. this can be verified in the following issues:
+        // https://github.com/tokio-rs/tracing/issues/575
+        // https://github.com/tokio-rs/tracing/issues/1708
+        //
+        // but there is a workaround described here:
+        // https://github.com/tokio-rs/tracing/issues/894
+        //
+        // the workaround consists of passing a optional of Layer to every conditional layer,
+        // so if Some(layer) is passed, that layer is active, if None the layer is inactive.
+        let (layer_format_json, layer_format_pretty, layer_format_hierarchical) =
+            match config.tracing.format {
+                TracingFormat::None => (None, None, None),
+                TracingFormat::Json => (Some(Layer::default().json()), None, None),
+                TracingFormat::Pretty => (
+                    None,
+                    Some(
+                        Layer::default()
+                            .pretty()
+                            .with_thread_ids(true)
+                            .with_thread_names(true)
+                            .with_target(true)
+                            .with_file(false)
+                            .with_line_number(false)
+                            .with_ansi(!config.core.no_color),
+                    ),
+                    None,
+                ),
+                TracingFormat::Hierarchical => (
+                    None,
+                    None,
+                    Some(
+                        HierarchicalLayer::new(2)
+                            .with_targets(true)
+                            .with_bracketed_fields(true)
+                            .with_ansi(!config.core.no_color),
+                    ),
+                ),
+            };
 
-            registry.with(telemetry).init();
-        }
+        Registry::default()
+            .with(EnvFilter::from_default_env())
+            .with(layer_format_json)
+            .with(layer_format_pretty)
+            .with(layer_format_hierarchical)
+            .with(telemetry)
+            .init();
 
         tracing::debug!("started tracer");
 
@@ -58,4 +104,12 @@ impl Drop for Tracing {
         tracing::debug!("stopping tracer");
         opentelemetry::global::shutdown_tracer_provider();
     }
+}
+
+#[derive(clap::ArgEnum, Clone, Debug)]
+pub enum TracingFormat {
+    None,
+    Hierarchical,
+    Pretty,
+    Json,
 }
