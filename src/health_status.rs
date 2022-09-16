1fef503d4f8d52d11af6e86d1bc7dc7ee7dcb4f0
Francisco Vicenzi <francisco.vicenzi@cloudwalk.io>
2022-09-16 16:23:52 -0300

    Add helper to handle health status checks

---
+++ src/health_status.rs
@@ -0,0 +1,121 @@
+use std::{
+    fmt::Display,
+    future::Future,
+    time::{Duration, Instant},
+};
+
+use eyre::Error;
+use serde::Serialize;
+use tokio::time::timeout;
+
+#[derive(Debug, Serialize)]
+pub struct HealthStatusReport {
+    pub status: HealthStatus,
+    pub duration: Duration,
+}
+
+#[derive(Debug, Serialize, PartialEq)]
+pub enum HealthStatus {
+    Healthy,
+    Degraded,
+    Offline { error: String },
+}
+
+impl Display for HealthStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HealthStatus::Healthy => write!(f, "healthy"),
+            HealthStatus::Degraded => write!(f, "degraded"),
+            HealthStatus::Offline { error: _ } => write!(f, "offline"),
+        }
+    }
+}
+
+impl HealthStatusReport {
+    pub async fn check_with_timeout_and_degrade<F>(
+        fut: F,
+        timeout_ms: u64,
+        degrade_ms: u64,
+    ) -> HealthStatusReport
+    where
+        F: Future<Output = Result<(), Error>>,
+    {
+        let start = Instant::now();
+
+        let status = match timeout(Duration::from_millis(timeout_ms), fut).await {
+            Ok(result) => match result {
+                Ok(()) => {
+                    let elapsed = start.elapsed();
+
+                    if elapsed > Duration::from_millis(degrade_ms) {
+                        HealthStatus::Degraded
+                    } else {
+                        HealthStatus::Healthy
+                    }
+                }
+                Err(e) => HealthStatus::Offline {
+                    error: e.to_string(),
+                },
+            },
+            Err(e) => HealthStatus::Offline {
+                error: e.to_string(),
+            },
+        };
+        HealthStatusReport {
+            status,
+            duration: start.elapsed(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use eyre::eyre;
+    use tokio::time::sleep;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn health_status_check() {
+        let future = || async move {
+            sleep(Duration::from_millis(100)).await;
+            Ok::<(), eyre::Error>(())
+        };
+
+        // timeout_ms = 200, degraded_ms = 150
+        // future sleeps for 100 ms so should be healthy
+        let report = HealthStatusReport::check_with_timeout_and_degrade(future(), 200, 150).await;
+        assert!(matches!(report.status, HealthStatus::Healthy));
+
+        // timeout_ms = 200, degraded_ms = 50
+        // future sleeps for 100 ms so should be degraded
+        let report = HealthStatusReport::check_with_timeout_and_degrade(future(), 200, 50).await;
+        assert!(matches!(report.status, HealthStatus::Degraded));
+
+        // timeout_ms = 50, degraded_ms = 30
+        // future sleeps for 100 ms so should be offline with matching message
+        let report = HealthStatusReport::check_with_timeout_and_degrade(future(), 50, 30).await;
+        assert!(matches!(report.status, HealthStatus::Offline { error: _ }));
+        assert_eq!(
+            report.status,
+            HealthStatus::Offline {
+                error: "deadline has elapsed".to_string()
+            }
+        );
+
+        let future = || async move {
+            sleep(Duration::from_millis(100)).await;
+            Err::<(), eyre::Error>(eyre!("test error!".to_string()))
+        };
+        // timeout_ms = 50, degraded_ms = 30
+        // future sleeps for 100 ms but return an error, so should be offline with matching message
+        let report = HealthStatusReport::check_with_timeout_and_degrade(future(), 150, 10).await;
+        assert!(matches!(report.status, HealthStatus::Offline { error: _ }));
+        assert_eq!(
+            report.status,
+            HealthStatus::Offline {
+                error: "test error!".to_string()
+            }
+        );
+    }
+}
